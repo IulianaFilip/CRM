299ffe2a5e889742c9977c0f6ca02e2b63794f2c
IulianaFilip <iuliana_ro2005@yahoo.com>
2023-09-25 08:04:41 +0300

    add models.Cascada

--- crm/crm/sales/models.py
+++ crm/crm/sales/models.py
@@ -8,7 +8,7 @@ class Leads(models.Model):
     status = models.CharField(max_length=100, choices=[('New', 'New'), ('Contacted', 'Contacted'), ('Converted', 'Converted'), ('Lost', 'Lost')])
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=100, blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.lead
@@ -35,9 +35,9 @@ class Accounts(models.Model):
 
 class Deals(models.Model):
     deal = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    contact = models.ForeignKey(Contacts, on_delete=models.SET_NULL, null=True)
-    account = models.ForeignKey(Accounts, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
+    account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
     stage = models.CharField(max_length=100, choices=[('Discovery', 'Discovery'), ('Proposal', 'Proposal'), ('Negotiation', 'Negotiation')])
     value = models.DecimalField(max_digits=10, decimal_places=2)
 
