a443f5f20603a851480a415c80f557154076ca33
IulianaFilip <iuliana_ro2005@yahoo.com>
2023-09-26 09:16:16 +0300

    rectify url path

--- crm/config/urls.py
+++ crm/config/urls.py
@@ -8,11 +8,11 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
-from crm.sales.views import lead
+from sales.views import leads
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("<int:lead_id>", lead,  name="lead"),
+    path("<int:lead_id>", leads ,  name="leads"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
