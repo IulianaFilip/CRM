cc83bba46976ccd582e2cb2b368652b7f8d63b0a
IulianaFilip <78206155+IulianaFilip@users.noreply.github.com>
2024-03-27 19:11:51 +0200

    Update urls.py

--- crm/sales/urls.py
+++ crm/sales/urls.py
@@ -5,6 +5,6 @@ from .views import home
 app_name = "sales"
 
 urlpatterns = [
-    path("", home, name="home"),  # Home view specific to the sales app
-    # More app-specific views go here
+    path("", home, name="home"), 
+  
 ]
