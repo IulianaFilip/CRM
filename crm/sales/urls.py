aa92721bde3d52b8906d48470913c110eb2b9364
IulianaFilip <iuliana_ro2005@yahoo.com>
2024-05-13 11:30:17 +0300

    updated files with html and css

--- crm/sales/urls.py
+++ crm/sales/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import dashboard_view, home
 
 app_name = "sales"
 
 urlpatterns = [
-    path("", home, name="home"),  # Home view specific to the sales app
-    # More app-specific views go here
+    path("", home, name="home"),
+    path("sales/", dashboard_view, name="dashboard"),
 ]
